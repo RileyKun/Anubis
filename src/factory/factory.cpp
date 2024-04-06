062bdde0b645d48365bf54a7a632f48ee9925d38
Riley <Riley@trigon.systems>
2024-04-05 22:14:14 -0500

    Initialize interfaces.

--- src/factory/factory.cpp
+++ src/factory/factory.cpp
@@ -1,5 +1,7 @@
 #include "factory.hpp"
 
+#include "link.hpp"
+
 void c_factory::startup() {
   interfaces();
   hooks(true);
@@ -17,4 +19,8 @@ void c_factory::hooks(bool startup) {
 }
 
 void c_factory::interfaces() {
+  // TODO: add assertions
+  g_tf2.hl_client = memory::find_interface<c_hl_client*>("client.dll", "VClient017");
+  g_tf2.engine_client = memory::find_interface<c_engine_client*>("engine.dll", "VEngineClient013");
+  g_tf2.surface = memory::find_interface<i_surface*>("vguimatsurface.dll", "VGUI_Surface030");
 }
