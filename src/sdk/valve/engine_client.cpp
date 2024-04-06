9d4abc51917ece5e76fa00224d72aa4b299d54b9
Riley <Riley@trigon.systems>
2024-04-06 00:49:03 -0500

    Populate c_engine_client

---
+++ src/sdk/valve/engine_client.cpp
@@ -0,0 +1,52 @@
+#include "engine_client.hpp"
+
+#include "link.hpp"
+
+void c_engine_client::get_screen_size(int& w, int& h) {
+  memory::find_vfunc<void(__thiscall*)(void*, int&, int&)>(this, 5u)(this, w, h);
+}
+
+void c_engine_client::client_cmd(const char* input) {
+  return memory::find_vfunc<void(__thiscall*)(void*, const char*)>(this, 12u)(this, input);
+}
+
+void c_engine_client::client_cmd_unrestricted(const char* input) {
+  // 100 something i don't know
+  // TODO: this function
+}
+
+int c_engine_client::get_local_player(void) {
+  return memory::find_vfunc<int(__thiscall*)(void*)>(this, 12u)(this);
+}
+
+float c_engine_client::plat_float_time(void) {
+  return memory::find_vfunc<float(__thiscall*)(void*)>(this, 14u)(this);
+}
+
+void c_engine_client::get_view_angles(QAngle& va) {
+  memory::find_vfunc<void(__thiscall*)(void*, QAngle&)>(this, 19u)(this, va);
+}
+
+void c_engine_client::set_view_angles(QAngle& va) {
+  memory::find_vfunc<void(__thiscall*)(void*, QAngle&)>(this, 20u)(this, va);
+}
+
+int c_engine_client::get_max_clients(void) {
+  return memory::find_vfunc<int(__thiscall*)(void*)>(this, 21u)(this);
+}
+
+bool c_engine_client::is_in_game(void) {
+  return memory::find_vfunc<bool(__thiscall*)(void*)>(this, 26u)(this);
+}
+
+bool c_engine_client::is_connected(void) {
+  return memory::find_vfunc<bool(__thiscall*)(void*)>(this, 27u)(this);
+}
+
+const char* c_engine_client::get_game_directory(void) {
+  return memory::find_vfunc<const char*(__thiscall*)(void*)>(this, 35u)(this);
+}
+
+const matrix3x4& c_engine_client::world_to_screen_matrix(void) {
+  return memory::find_vfunc<const matrix3x4&(__thiscall*)(void*)>(this, 36u)(this);
+}
--- src/sdk/valve/engine_client.hpp
+++ src/sdk/valve/engine_client.hpp
@@ -1,6 +1,22 @@
 #pragma once
 
+#include "mathlib.hpp"
+
 class c_engine_client {
 public:
-
+  void get_screen_size(int& w, int& h);
+  void client_cmd(const char* input);
+  void client_cmd_unrestricted(const char* input);
+  int get_local_player(void);
+  float plat_float_time(void);
+  void get_view_angles(QAngle& va);
+  void set_view_angles(QAngle& va);
+  int get_max_clients(void);
+  // is not loading anymore but is connected
+  bool is_in_game(void);
+  // is connected but still loading
+  bool is_connected(void);
+  // returns the game's root directory
+  const char* get_game_directory(void);
+  const matrix3x4& world_to_screen_matrix(void);
 };
