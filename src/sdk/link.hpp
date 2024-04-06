fbf76282501ebb03721156e55eec997a9719a147
Riley <Riley@trigon.systems>
2024-04-06 18:03:36 -0500

    Migrate ctx to link.hpp

--- src/sdk/link.hpp
+++ src/sdk/link.hpp
@@ -9,7 +9,6 @@
 #include "valve/user_cmd.hpp"
 
 // include cheat headers
-#include "shared/ctx.hpp"
 #include "shared/memory.hpp"
 
 // store all interfaces of the game
@@ -20,6 +19,12 @@ public:
   c_engine_vgui*        engine_vgui = nullptr;
   i_surface*            surface = nullptr;
   i_client_mode_shared* client_mode = nullptr;
+class c_ctx {
+public:
+  c_tf_player* local_player = nullptr;
+  
+  void get_local();
 };
 
+inline auto  ctx = std::unique_ptr<c_ctx>();
 inline c_tf2 g_tf2{};
