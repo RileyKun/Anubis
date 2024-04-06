fac4683b32871b9a31ec657ba37a293682bea34e
Riley <Riley@trigon.systems>
2024-04-06 00:06:19 -0500

    Add new classes to link file

--- src/sdk/link.hpp
+++ src/sdk/link.hpp
@@ -1,22 +1,25 @@
 #pragma once
 
 // include sdk headers
+#include "valve/client_mode.hpp"
 #include "valve/engine_client.hpp"
 #include "valve/engine_vgui.hpp"
 #include "valve/hl_client.hpp"
 #include "valve/surface.hpp"
+#include "valve/user_cmd.hpp"
 
 // include cheat headers
-#include "hack/ctx.hpp"
-#include "hack/memory.hpp"
+#include "shared/ctx.hpp"
+#include "shared/memory.hpp"
 
 // store all interfaces of the game
 class c_tf2 {
 public:
-  c_engine_client* engine_client = nullptr;
-  c_hl_client*     hl_client = nullptr;
-  c_engine_vgui*   engine_vgui = nullptr;
-  i_surface*       surface = nullptr;
+  c_engine_client*      engine_client = nullptr;
+  c_hl_client*          hl_client = nullptr;
+  c_engine_vgui*        engine_vgui = nullptr;
+  i_surface*            surface = nullptr;
+  i_client_mode_shared* client_mode = nullptr;
 };
 
 inline c_tf2 g_tf2{};
