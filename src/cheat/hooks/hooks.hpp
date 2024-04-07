4422b12a710999644a4c35d8278ab2ac979e8e59
Riley <Riley@trigon.systems>
2024-04-07 05:15:24 -0500

    Create surface hook

--- src/cheat/hooks/hooks.hpp
+++ src/cheat/hooks/hooks.hpp
@@ -2,6 +2,7 @@
 
 class c_user_cmd;
 enum frame_stage;
+enum mode_t;
 namespace hooks {
 
 namespace createmove {
@@ -14,6 +15,13 @@ namespace fs_notify {
 void startup();
 void shutdown();
 void __fastcall hooked_fs_notify(void* ecx, void* edx, frame_stage stage);
-}
+} // namespace fs_notify
+
+namespace paint {
+void startup();
+void shutdown();
+void __fastcall hooked_paint(void* ecx, void* edx, mode_t mode);
+void __fastcall hooked_size_changed(void* ecx, void* edx, int old_width, int old_height);
+} // namespace paint
 
 } // namespace hooks
