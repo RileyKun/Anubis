7207bd178220695a1cd9fe933270270cfec12479
Riley <Riley@trigon.systems>
2024-04-07 03:20:47 -0500

    Fix a crash relating to localplayer being null

--- src/sdk/link.cpp
+++ src/sdk/link.cpp
@@ -2,5 +2,6 @@
 
 // cleans up frame_stage_notify
 void c_ctx::get_local() {
-  g_tf2.entity_list->get_client_entity(g_tf2.engine_client->get_local_player())->as<c_tf_player>();
+  local_player = g_tf2.entity_list->get_client_entity(g_tf2.engine_client->get_local_player())
+                     ->as<c_tf_player>();
 }
