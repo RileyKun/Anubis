2bde261b51600496e70a61dc208d6b626711b1f5
Riley <Riley@trigon.systems>
2024-04-07 05:24:50 -0500

    Initialize surface hook

--- src/factory/factory.cpp
+++ src/factory/factory.cpp
@@ -16,9 +16,11 @@ void c_factory::hooks(bool startup) {
   if(startup) {
     hooks::createmove::startup();
     hooks::fs_notify::startup();
+    hooks::paint::startup();
   } else {
     hooks::createmove::shutdown();
     hooks::fs_notify::shutdown();
+    hooks::paint::shutdown();
   }
 }
 
