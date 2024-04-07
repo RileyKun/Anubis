079e6011afc9f34a4f83fc86cb144662488fba66
Riley <Riley@trigon.systems>
2024-04-07 05:24:36 -0500

    Write surface hook

---
+++ src/cheat/hooks/lists/paint.cpp
@@ -0,0 +1,41 @@
+#include "../hooks.hpp"
+#include "link.hpp"
+
+#include <safetyhook.hpp>
+
+#include <mutex>
+
+SafetyHookInline pt{};
+SafetyHookInline size_change{};
+
+std::once_flag   init;
+
+void             hooks::paint::startup() {
+  pt = safetyhook::create_inline(memory::vfunc_ptr(g_tf2.engine_vgui, 14), hooked_paint);
+  size_change =
+      safetyhook::create_inline(memory::vfunc_ptr(g_tf2.surface, 111), hooked_size_changed);
+}
+
+void hooks::paint::shutdown() {
+  pt = {};
+  size_change = {};
+}
+
+void __fastcall hooks::paint::hooked_paint(void* ecx, void* edx, mode_t mode) {
+  pt.thiscall<void>(ecx, mode);
+  std::call_once(init, [&] {
+    g_tf2.engine_client->get_screen_size(ctx->screen_width, ctx->screen_height);
+  });
+
+  if(mode & mode_t::PAINT_UIPANELS) {
+    g_tf2.surface->start();
+    {}
+    g_tf2.surface->stop();
+  }
+}
+
+void __fastcall hooks::paint::hooked_size_changed(void* ecx, void* edx, int old_width,
+                                                  int old_height) {
+  size_change.thiscall<void>(ecx, old_width, old_height);
+  g_tf2.engine_client->get_screen_size(ctx->screen_width, ctx->screen_height);
+}
