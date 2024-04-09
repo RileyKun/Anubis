244c13bf832d180f316139f9f96a4901daa2dd27
Riley <Riley@trigon.systems>
2024-04-08 19:22:35 -0500

    Add get_player_view method to CHLClient

--- src/sdk/valve/hl_client.cpp
+++ src/sdk/valve/hl_client.cpp
@@ -5,3 +5,7 @@
 ClientClass* c_hl_client::get_all_classes(void) {
   return memory::find_vfunc<ClientClass*(__thiscall*)(void*)>(this, 8u)(this);
 }
+
+bool c_hl_client::get_player_view(c_view_setup& player_view) {
+  return memory::find_vfunc<bool(__thiscall*)(void*, c_view_setup&)>(this, 59u)(this, player_view);
+}
--- src/sdk/valve/hl_client.hpp
+++ src/sdk/valve/hl_client.hpp
@@ -2,8 +2,10 @@
 
 #include "datatables.hpp"
 #include "frame_stage.hpp"
+#include "view_setup.hpp"
 
 class c_hl_client {
 public:
   ClientClass* get_all_classes(void);
+  bool get_player_view(c_view_setup& player_view);
 };
