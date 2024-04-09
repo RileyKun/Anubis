97b3eeac1bb33333cdc96947a5ea951754339cc5
Riley <Riley@trigon.systems>
2024-04-08 19:29:49 -0500

    Add settings namespace

---
+++ src/cheat/features/config.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace settings {
+
+namespace visuals {
+
+namespace esp {
+inline bool enabled = true;
+inline bool draw_players = true;
+} // namespace esp
+
+
+
+} // namespace visuals
+
+} // namespace settings
