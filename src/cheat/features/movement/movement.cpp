9c42b88941dcdfa4a1fbe7008a9b96b72a8644b0
Viceroy <97338859+Viceroyy@users.noreply.github.com>
2024-04-10 23:00:06 +0300

    Allowed bunnyhopping as scout :)

--- src/cheat/features/movement/movement.cpp
+++ src/cheat/features/movement/movement.cpp
@@ -2,14 +2,21 @@
 
 #include "link.hpp"
 
-// FIXME: Riley; allow bunnyhopping as scout
 void c_movement::bunnyhop(c_tf_player* player, c_user_cmd* cmd) {
-  auto  flags = player->flags();
-  auto& buttons = cmd->buttons;
+  int  flags = player->flags();
+  int& buttons = cmd->buttons;
+  static bool jump_state = false;
 
-  if(!!(buttons & IN_JUMP) && !(flags & FL_ONGROUND)) {
-    buttons &= ~IN_JUMP;
+  if(buttons & IN_JUMP) {
+    if(!jump_state && !(flags & FL_ONGROUND))
+      buttons &= ~IN_JUMP;
+
+    else if(jump_state)
+      jump_state = false;
   }
+
+  else if(!jump_state)
+    jump_state = true;
 }
 
 void c_movement::run(c_user_cmd* cmd) {
